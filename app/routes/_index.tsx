885e6217a08d61bb7030f4e58f0c13defd69cef3
imskanand <the.anand3099@gmail.com>
2023-12-06 17:22:12 +0530

    Simple UI fix and Doc update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Togged." }, { name: "description", content: "Tracking Your Day." }];
@@ -6,8 +7,11 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="text-green-700 font-bold text-xl">
-      <h1>toggler</h1>
+    <div className="min-h-screen mt-16 mx-48 text-gray-300 flex flex-col gap-2">
+      <h1 className="text-xl font-bold">Toggler</h1>
+      <Link to="/notes" className="underline" prefetch="intent">
+        Notes
+      </Link>
     </div>
   );
 }
